bf8dd24c45d347e5bdbfc37d6a764b7cda1f1809
MimithaPC <mimithaprabodani@gmail.com>
2025-08-09 14:13:09 +0530

    First

---
+++ src/App.tsx
@@ -0,0 +1,131 @@
+function App() {
+
+    return (
+   
+        <div className="full-screen bg-gray-100">
+            <div className="p-10">
+                <h1 className="font-bold text-4xl text-center">RAJAPAKSHAGE MIMITHA PRABODANI CHANDRARATHNA</h1>
+                <h1 className="font-bold text-2xl text-center">Intern Software Engineer</h1>
+                <h1 className="font-bold text-2xl text-center space-x-4">
+  <a href="tel:+94764912257" className="hover:underline">+94 76 491 2257</a> | 
+  <a href="mailto:mimithaprabodani@gmail.com" className="hover:underline">mimithaprabodani@gmail.com</a> | 
+  <a href="https://www.linkedin.com/in/mimitha-prabodani-22b54134a" target="_blank" rel="noopener noreferrer" className="hover:underline">LinkedIn</a> | 
+  <a href="https://github.com/MimithaPC" target="_blank" rel="noopener noreferrer" className="hover:underline">GitHub</a> | 
+  <a href="https://yourportfolio.com" target="_blank" rel="noopener noreferrer" className="hover:underline">Portfolio</a>
+</h1>
+            </div>
+             <hr></hr>
+             <div className="mx-10">
+                 <div className="p-10">
+                     <h1 className="font-bold text-3xl underline">OBJECTIVE</h1>
+                     <br></br>
+                     <h1 className="text-2xl">I am currently an undergraduate student in Information Technology. I am passionate about software development and problem-solving. I am seeking a Software Engineering Intern position where I can apply my programming knowledge, improve my technical skills, and contribute to building efficient, scalable software solutions.</h1>
+                 </div>
+            <div className="p-10">
+           
+                 <h1 className="font-bold text-3xl underline">EDUCATION</h1>
+                 <br></br>
+                  <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">Higher National Diploma in Information Technology,</h1></div><div className="pl-28"><h1 className="text-2xl">(2023 – Present)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Sri Lanka Institute of Advanced Technological Education,</h1>
+                   <h1 className="text-2xl">Kurunegala.</h1>
+                   </div>
+
+                   <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">Certificate Course in Web Designing,</h1></div><div className="pl-72"><h1 className="text-2xl">(November 2023)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Information and Communication Technology Centre,</h1>
+                   <h1 className="text-2xl">University of Kelaniya.</h1>
+                   </div>
+
+                   <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">Advanced Certificate in Counseling Psychology,</h1></div><div className="pl-40"><h1 className="text-2xl">(November 2021)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Department of Psychology,</h1>
+                    <h1 className="text-2xl">Faculty of Arts,</h1>
+                   <h1 className="text-2xl">University of Peradeniya.</h1>
+                   </div>
+
+                   <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">Certificate in Psychology and Basic Counseling Skills,</h1></div><div className="pl-24"><h1 className="text-2xl">(September 2020)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Department of Psychology,</h1>
+                    <h1 className="text-2xl">Faculty of Arts,</h1>
+                   <h1 className="text-2xl">University of Peradeniya.</h1>
+                   </div>
+
+                   <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">Certificate Course in MS-OFFICE,</h1></div><div className="pl-80"><h1 className="text-2xl">(August 2020)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Vijayanandha Nenasala,</h1>
+                   <h1 className="text-2xl">Polgahawela.</h1>
+                   </div>
+
+                   <div className="flex"><div><h1 className="text-2xl font-bold list-item list-disc pl-6 ml-6">General Certificate of Education (A/L) Examination,</h1></div><div className="pl-28"><h1 className="text-2xl">(May 2018)</h1></div></div>
+                   <div className="pl-12">
+                    <h1 className="text-2xl">Kegalu Balika Vidyalaya,</h1>
+                   <h1 className="text-2xl">Kegalle.</h1>
+                   </div>
+            </div>
+           
+            <div className="p-10">
+           
+                 <h1 className="font-bold text-3xl underline">SKILLS</h1>
+                 <br></br>
+                
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Programming Languages - Java, JavaScript, Php, .NET, C#, TypeScript</h1>
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Database Management - MySQL, PostgreSQL</h1>
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Version Control - GitHub</h1>
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Web Development - HTML, CSS, Tailwind CSS, React.js, Node.js, Express.js</h1>
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Software Development Concepts - Object-Oriented Programming (OOP), Data Structures and Algorithms</h1>
+                    <h1 className="text-2xl list-item list-disc pl-6 ml-6">Tools - Postman and Thunder Client (API testing), Figma</h1>
+                    
+                  
+            </div>
+            <div className="p-10">
+           
+                 <h1 className="font-bold text-3xl underline">LANGUAGES</h1>
+                 <br></br>
+                 <h1 className="text-2xl list-item list-disc pl-6 ml-6">Sinhala - Fluent </h1>
+                <h1 className="text-2xl list-item list-disc pl-6 ml-6">English - Intermediate</h1>
+                
+            </div>
+            <div className="p-10">
+           
+                 <h1 className="font-bold text-3xl underline">PROJECTS</h1>
+                 <br></br>
+                  <h1 className="text-2xl"></h1>
+            </div>
+            <div className="p-10">
+           
+                 <h1 className="font-bold text-3xl underline">REFERENCES</h1>
+                 <br></br>
+                  <div className="flex p-10">
+                 <div className="mr-96">
+                    <h1 className="text-2xl">Ms.K.G.D.De.A. Wijesinghe </h1>
+                    <h1 className="text-2xl">HOD - SLIATE Kurunegala </h1>
+                     <h1 className="text-2xl">
+  <a href="mailto:wijesinghe1975@gmail.com" className="hover:underline">
+    wijesinghe1975@gmail.com
+  </a>
+</h1>
+                       <h1 className="text-2xl">+94 77 336 4643</h1>
+                 </div>
+                  <div>
+                    <h1 className="text-2xl">Mr.K.W.G.S. Konthasinghe</h1>
+                     <h1 className="text-2xl">Lecturer - SLIATE Kurunegala</h1>
+                     <h1 className="text-2xl">
+  <a href="mailto:Gayans@sliate.ac.lk" className="hover:underline">
+    Gayans@sliate.ac.lk
+  </a>
+</h1>
+
+                       <h1 className="text-2xl">+94 77 941 7938</h1>
+                  </div>
+                  </div>
+                 
+                  
+            </div>
+            </div>
+        </div>
+      
+    )
+}
+
+export default App
